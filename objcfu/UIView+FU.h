02f5dabb0cb657c52b32d73927f59a2cb97a51bf
johnnyfuchs <johnnyfuchs@gmail.com>
2015-02-26 09:08:49 -0800

    Adding uiview category

---
+++ objcfu/UIView+FU.h
@@ -0,0 +1,22 @@
+//
+// Created by Johnny Sparks on 2/26/15.
+// Copyright (c) 2015 Johnny Sparks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface UIView (FU)
+
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize size;
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+
+@end
